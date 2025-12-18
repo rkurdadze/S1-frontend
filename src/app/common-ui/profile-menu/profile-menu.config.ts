7f3969bb291f90c99f9aa0e7072be3d6d19d3965
Roman Kurdadze <romankurdadze@gmail.com>
2025-12-18 17:55:32 +0400

    purchase bar v1

--- src/app/common-ui/profile-menu/profile-menu.config.ts
+++ src/app/common-ui/profile-menu/profile-menu.config.ts
@@ -1,21 +1,21 @@
 import { ProfileMenuItem } from './profile-menu.types';
 
 export const PROFILE_MENU_ITEMS: ProfileMenuItem[] = [
-  {
-    labelKey: 'mobileNav.home',
-    icon: 'home',
-    routerLink: '/'
-  },
-  {
-    labelKey: 'mobileNav.catalog',
-    icon: 'catalog',
-    routerLink: '/catalog'
-  },
-  {
-    labelKey: 'mobileNav.cart',
-    icon: 'cart',
-    routerLink: '/cart'
-  },
+  // {
+  //   labelKey: 'mobileNav.home',
+  //   icon: 'home',
+  //   routerLink: '/'
+  // },
+  // {
+  //   labelKey: 'mobileNav.catalog',
+  //   icon: 'catalog',
+  //   routerLink: '/catalog'
+  // },
+  // {
+  //   labelKey: 'mobileNav.cart',
+  //   icon: 'cart',
+  //   routerLink: '/cart'
+  // },
   {
     labelKey: 'dropdown.profileSettings',
     icon: 'profile',
